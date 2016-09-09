259e5335afc063cd6fd6438661ce4f34843c6ebe
urnmo <aaron0moore@gmail.com>
2016-09-09 11:11:52 -0400

    add jquery and lodash

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-
+let _ = require('lodash');
 
 
 //write the ajax request function
@@ -10,14 +9,14 @@ function getPeeps() {
     request.open('GET', "https://damp-hamlet-50601.herokuapp.com/people");
 
     request.addEventListener('load', function () {
-
         //get the json and parse it
         let response = JSON.parse(request.responseText);
 console.log (response);
     })
-        request.send();
-        
+        request.send();  
 }
+
+
 
 
 
@@ -36,3 +35,19 @@ window.addEventListener('load', function() {
 console.log ('hello world');
 getPeeps();
 });
+// -populate the "friends" box--js 
+//      -parse json
+//      -pass data into a constructor
+
+// -make  'friends'draggable and droppable--jquery
+
+// -set up post fuction--js
+    //-- on drop
+    //--set up "codename" property and function
+    
+
+// -make boxes all scrollable --css
+
+// --display group type on friend
+
+
