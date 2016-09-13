90473c33a15d2c9735a03e015948839a9bde67e5
urnmo <aaron0moore@gmail.com>
2016-09-13 09:20:01 -0400

    flux

--- app.js
+++ app.js
@@ -4,8 +4,9 @@
 //write the ajax request function
 
 
-function getPeeps() {         // GET PPL OBJECTS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-    let request = new XMLHttpRequest();
+function getPeeps() {         // GET PPL OBJECTS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
+// -populate the "friends" box--js 
+let request = new XMLHttpRequest();
     request.open('GET', "https://damp-hamlet-50601.herokuapp.com/people");
 
     request.addEventListener('load', function () {
@@ -14,7 +15,7 @@ function getPeeps() {         // GET PPL OBJECTS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
         console.log(response);
         for (let i = 0; i < response.length; i++) {
 
-            $('#friends-list').append('<li class="peep"><p><img src="' + response[i].photo + '"></p><h2 class="friendsinfo">' + response[i].firstName + ' ' + response[i].lastName + '</h2><p class="username friendsinfo">' + response[i].userName + '</p><p class="friendsinfo">' + response[i].gender + '</p><p class="friendsinfo">' + response[i].birthday + '</p></li>');
+            $('#friends-list').append('<li class="peep"><p><img src="' + response[i].photo + '"></p><h2 class="friendsinfo">' + response[i].firstName + ' ' + response[i].lastName + '</h2><p class="username friendsinfo">' + response[i].userName + '</p><p class="friendsinfo">' + response[i].gender + '</p><p class="friendsinfo">' + response[i].birthday + '</p><p class="hidden">' + response[i].id + '</p></li>');
 
     
         }
@@ -31,7 +32,7 @@ function getPeeps() {         // GET PPL OBJECTS @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
             drop: function (event, ui) {
                 console.log('drop');
                 // peep should be the user id. 
-                let peep = ui.draggable.find('.username').text();
+                let peep = ui.draggable.find('.id').text();
                 let group = "";
                 $(this);
                 console.log($(this).attr('id'));
@@ -70,10 +71,14 @@ function getGroups() {
         //get the json and parse it
         let response = JSON.parse(request.responseText);
         console.log(response);
+
+        //render all DOM elements inside 
+
         for (let i = 0; i < response.length; i++) {
             let groupName = response[i].groupName;
             $('#groups-box').append('<div class="groups ' + groupName + 's-group"><div id="' + groupName + '" class="group-boxes ' + groupName + 's-box"><ul class="peepsNgroup"></ul></div></div>')
-        for (let j = 0; j < response[i].people.length; j++){
+        for (let j = 0; j < response[i].peopleIds.length; j++){
+            
             $('#'+ groupName + ' ul').append('<li class="peepNgroup">' + response[i].people[j].firstName + ' ' + response[i].people[j].lastName + '</li>')
         }}
         console.log("groups sent")
@@ -95,19 +100,19 @@ function getGroups() {
 //     request.send();
 // }
 
+// -set up post fuction--js
+    //-- on drop
 function postGroup(user, group) {
     console.log('what were sending: ' + user + ', ' + group);
     // post the group info
     let request = new XMLHttpRequest();
-    request.open('POST', "https://damp-hamlet-50601.herokuapp.com/" + group + "/" + user);
-    let msg = user;
-    // request.send(JSON.stringify({ user: msg }));
-  console.log  (JSON.stringify({
+    request.open('POST', "https://damp-hamlet-50601.herNOPEokuapp.com/" + group + "/" + user);
+    request.send();
+    console.log  (JSON.stringify({
       user: msg,
     }));
 
     console.log(group);
-    console.log(msg);
 
     // ---msg needs to be an object with user id and group id
 
@@ -123,28 +128,37 @@ window.addEventListener('load', function () {
     console.log('hello world');
     getPeeps();
     getGroups();
-
-
 });
 
 
+        // document.querySelector("Search-box")
 
 
-// -populate the "friends" box--js 
-//      -parse json
-//      -pass data into a constructor
+//set the search button as a variable
+let submitBtn = document.querySelector("srchBtn");
+//add a click listener for it that sends a get request with the variable userInput on end
+submitBtn.addEventListener('click', function (){
+    let request = new XMLHttpRequest();
+    request.open('GET', "https://damp-hamlet-50601.herokuapp.com/people/?search=" + userInput);
+    //add eventlistener looking for load
+    request.addEventListener('load', function(){
+ //parse the response
+    let response = JSON.parse(request.responseText);
+        console.log(response);
+    })
 
-// -make  'friends'draggable and droppable--jquery
-        //render a li for each peeps
+
+}
+}
+    });
 
 
 
 
 
 
-// -set up post fuction--js
-    //-- on drop
-    //--set up "codename" property and function (maybe)
+
+
 
 // --display group type on friend
 
